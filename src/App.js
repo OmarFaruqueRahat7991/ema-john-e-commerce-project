2800b5c06cb27c4044d04f12c60110202e19eba1
OmarFaruqueRahat7991 <rahatomarfaruque@gmail.com>
2022-10-08 07:18:12 +0600

    final commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
-import Header from './Components/Header/Header';
+import About from './Components/About/About';
+import Inventory from './Components/Inventory/Inventory';
+import Orders from './Components/Orders/Orders';
 import Shop from './Components/Shop/Shop';
+import Main from './Layouts/Main';
+import { ProductsAndCartLoader } from './Loaders/ProductsAndCartLoader';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Main></Main>,
+        children: [
+          {
+            path: '/',
+            loader: () =>  fetch('products.json') ,
+            element: <Shop></Shop>
+          },
+          {
+            path: 'orders',
+            loader: () =>  ProductsAndCartLoader(),
+            element: <Orders></Orders>
+          },
+          {
+            path: 'inventory',
+            element: <Inventory></Inventory>
+          },
+          {
+            path: 'about',
+            element: <About></About>
+        },
+        ]
+    }
+    
+]);
   return (
     <div>
-      <Header></Header>
-      <Shop></Shop>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
